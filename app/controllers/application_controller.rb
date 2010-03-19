6c570fead9b6983d90d5d9ede5ac4edb5ab6be77
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-03-19 13:27:15 +0000

    Initial style sheet integration and translation from static design to application layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,34 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  #localization functions
+  before_filter :set_locale
+
+  def set_locale
+    if Rails.env.test? || request.subdomains.first.nil?
+      if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+        cookies['locale'] = { :value => params[:locale], :expires => 1.year.from_now }
+        I18n.locale = params[:locale].to_sym
+      elsif cookies['locale'] && I18n.available_locales.include?(cookies['locale'].to_sym)
+        I18n.locale = cookies['locale'].to_sym
+      end
+    else
+      I18n.locale = extract_locale_from_subdomain
+      puts "TRANSLATION LOCALE: " + I18n.locale.to_s
+    end
+  end
+
+  def extract_locale_from_subdomain
+    puts "REQUESTED DOMAIN: " + request.subdomains.first unless request.subdomains.first.nil?
+    parsed_locale = request.subdomains.first.to_sym
+    (I18n.available_locales.include? parsed_locale) ? parsed_locale  : nil
+  end
+
+  protected
+
+  def admin_required
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == 'admin' && password == 'Qajar'
+    end if Rails.env == 'production' || params[:admin_http]
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,86 @@
 module ApplicationHelper
+
+  def language_suffix
+    '_fa' if I18n.locale == :fa
+  end
+
+  def language_url
+    # TODO: Make this context sensitive -- hard coded for development
+    if I18n.locale == :fa
+      return 'http://es.example.com:3000'
+    else
+      return 'http://fa.example.com:3000'
+    end
+  end
+
+  def add_this_block
+    s = %{
+    <!-- AddThis Button BEGIN -->
+    <script type="text/javascript">var addthis_pub="mesolore";</script>
+    <a href="http://www.addthis.com/bookmark.php"
+            onmouseover="return addthis_open(this, '', '[URL]', '[TITLE]');"
+            onmouseout="addthis_close();"
+            onclick="return addthis_sendto();">#{t(:share)}</a>
+    <script type="text/javascript" src="http://s7.addthis.com/js/200/addthis_widget.js"></script>
+    <!-- AddThis Button END -->
+    }
+  end
+  def google_analytics_block
+    if Rails.env == 'production'
+      s = %{
+    <!-- Google Analytics BEGIN -->
+    <script type="text/javascript">
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+      try {
+        var pageTracker = _gat._getTracker("UA-1325142-22");
+        pageTracker._trackPageview();
+      } catch(err) {}
+    </script>
+    <!-- Google Analytics END -->
+      }
+    end
+  end
+
+  #zoomify javascript include block
+  def include_zoomify?
+    # check is we are on the source controller and have a valid source stored
+    return %{
+    <!-- Zoomify swfobject registration BEGIN -->
+    <script type="text/javascript">
+      swfobject.registerObject("zoomify_swf", "10.0.0", "/flash/expressInstall.swf");
+    </script>
+    <!-- Zoomify swfobject registration END -->
+    } unless @source.nil?
+  end
+
+  #fckeditor javascript include block
+  def include_fckeditor?
+    # check whether we are editing or need the control
+    return %{
+      javascript_include_tag 'fckeditor/fckeditor.js'
+    } if ['edit','new'].include?(controller.action_name) || @use_fckeditor
+  end
+
+  TYPO_TAG_KEY = TYPO_ATTRIBUTE_KEY = /[\w:_-]+/
+  TYPO_ATTRIBUTE_VALUE = /(?:[A-Za-z0-9]+|(?:'[^']*?'|"[^"]*?"))/
+  TYPO_ATTRIBUTE = /(?:#{TYPO_ATTRIBUTE_KEY}(?:\s*=\s*#{TYPO_ATTRIBUTE_VALUE})?)/
+  TYPO_ATTRIBUTES = /(?:#{TYPO_ATTRIBUTE}(?:\s+#{TYPO_ATTRIBUTE})*)/
+  TAG = %r{<[!/?\[]?(?:#{TYPO_TAG_KEY}|--)(?:\s+#{TYPO_ATTRIBUTES})?\s*(?:[!/?\]]+|--)?>}
+
+  #strip_html
+  def strip_html(html_string)
+    return html_string.gsub(TAG, '').gsub(/\s+/, ' ').strip
+  end
+
+  #blip.tv player
+  def blip_player(id)
+    return %{
+      <!-- blipTV player BEGIN -->
+      <embed src="http://blip.tv/play/#{id}" type="application/x-shockwave-flash" width="480" height="390" allowscriptaccess="always" allowfullscreen="true"></embed>
+      <!-- blipTV player END -->
+    } unless id.nil?
+  end
 end
