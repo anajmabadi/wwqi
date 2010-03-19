74b0f2f0a58f2869360fc203058a639dd1c9e389
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-03-19 10:19:55 +0000

    Initial code import for Rails 3

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    @welcome = "Hello World"
+  end
+
+end
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'f2789c98ca9b15647131866854dc6ed3b76fb668e5080a88225f099b056388f64fbe2a9be82743c2fb5c6af01625f43c8c0e7af0eb35eadabc997287367de046'
