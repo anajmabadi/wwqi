0ffd3c1cbd25f858e8f3be1bc28ab15a5619e61a
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-03-23 15:33:55 +0000

    Editing deploy method and caching required gems.

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+#############################################################
+#	Application
+#############################################################
+
+set :application, "qajar"
+set :domain,      "qajarwomen.org"
+set :deploy_to, "/var/www/vhosts/qajarwomen.org/httpdocs"
+
+#############################################################
+#	Settings
+#############################################################
+
+default_run_options[:pty] = true
+set :use_sudo, true
+
+#############################################################
+#	Subversion
+#############################################################
+
+set :scm, :subversion # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :repository,  "https://historicus.springloops.com/source/qajar/trunk"
+set :svn_username, "cforcey"
+set :svn_password, "kailiv14"
+set :checkout, "export"
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true        # This is where Rails migrations will run
+
+
+namespace :deploy do
+
+ task :start, :roles => :app do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+
+  task :stop do
+  # Do nothing.
+ end
+
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+
+end
