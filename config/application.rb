166b6290547ec122a437c559d86accaa2f759cd7
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-03-23 17:11:09 +0000

    Editing deploy method and caching required gems.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# This makes sure the bundled gems are in our $LOAD_PATH
+require File.expand_path(File.join(File.dirname(FILE), 'vendor', 'gems', 'environment'))
+
 # Auto-require default libraries and those for the current Rails environment.
 Bundler.require :default, Rails.env
 
