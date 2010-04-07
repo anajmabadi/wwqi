bf40023b4e9b97a3e0ef3fc47dfb70087596d62c
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-04-07 15:51:07 +0000

    Enabling the exhibits page

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,5 +1,7 @@
 class ExhibitsController < ApplicationController
   def index
+    @featured_exhibit = Exhibition.find(:first, :conditions => 'publish=1 AND featured=1')
+    @exhibits = Exhibition.find(:all, :conditions => 'publish=1')
   end
 
   def show
