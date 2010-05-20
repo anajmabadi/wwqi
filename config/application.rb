0cd291a870b414038eb2bbdcab9962b81775e687
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-05-20 17:25:00 +0000

    Removing the caching in the hope of seeing new translations.

--- config/application.rb
+++ config/application.rb
@@ -42,8 +42,8 @@ module Qajar
     # switching over to active record for translations
     I18n.backend = I18n::Backend::ActiveRecord.new
     I18n.backend.extend(I18n::Backend::Fallbacks) 
-    I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Cache)
-    I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+    #I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Cache)
+    #I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
     
   end
 end
