06b089ac0213dd3604b3b52a92632a22591af0eb
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-07-03 06:47:59 +0000

    getting cucumber tests to work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @about_page = Page.find(:first, :conditions => 'pages.id=1')
-    @featured_exhibit = Exhibition.find(:first, :conditions => 'featured=1')
+    @about_page = Page.find(:first, :conditions => 'pages.id=1') || Page.new
+    @featured_exhibit = Exhibition.find(:first, :conditions => 'featured=1') || Exhibition.new
   end
-
 end
