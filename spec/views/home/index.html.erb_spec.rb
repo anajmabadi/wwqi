8e2ee5615772618456f0eb4bfe91832281a2b05a
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-07-05 14:56:47 +0000

    more tests

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
+require 'home_helper'
 
 describe "home/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    assigns[:about_page] = stub("Page", :title => 'About', :body => '<p>Hello World!</p>')
+    assigns[:featured_exhibit] = stub("Exhibition", :title => 'Sample Exhibit', :description => "Sample Description", :featured => true)
+  end   
+  
+  it "displays the correct title and about message" do
+    render
+    response.should contain("About")
+    response.should containt("Hello World")
+  end  
+  
 end
