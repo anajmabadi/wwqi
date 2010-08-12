658ea4f0930b0a7597296aae1b5ad516dbe0cec8
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-08-12 05:45:07 +0000

    progress on browser code

--- app/helpers/archive_helper.rb
+++ app/helpers/archive_helper.rb
@@ -60,88 +60,111 @@ module ArchiveHelper
     }
   end
   
-  def header_content
+  def browser_header_content
     return %{
     <link href="/stylesheets/dd<%= language_suffix -%>.css" media="screen" rel="stylesheet" type="text/css" />
     <script src="/javascripts/uncompressed.jquery.dd.js" type="text/javascript"></script>
-    <link rel="stylesheet" type="text/css" href="/stylesheets/facebox<%= language_suffix -%>.css" media="screen" />
-    <script type="text/javascript" src="/javascripts/facebox.js" ></script>
+    <script src="/javascripts/jquery.qtip-1.0.0-rc3.min.js" type="text/javascript"></script> 
 
-    <script language="javascript" type="application/javascript">
-      $(document).ready(function(){
+  	<script language="javascript" type="application/javascript">
+  	$(document).ready(function(){
+  		$(".AZsort").click(function(){
+  		$(".AZsort").parent().siblings().removeClass("active");
+  		$(".AZsort").parent().addClass("active");
+  		$(".AZsort").parent().siblings().find("span").removeClass("up");
+  		$(".AZsort").find("span").toggleClass("up");
+  		return false;
+  	});
 
-      // click events for handling proper css classes sort buttons (A-Z / Date)
-      $(".AZsort").click(function(){
-      $(".AZsort").parent().siblings().removeClass("active");
-      $(".AZsort").parent().addClass("active");
-      $(".AZsort").parent().siblings().find("span").removeClass("up");
-      $(".AZsort").find("span").toggleClass("up");
-      return false;
-      });
+  	$(".dateSort").click(function(){
+  		$(".dateSort").parent().siblings().removeClass("active");
+  		$(".dateSort").parent().addClass("active");
+  		$(".dateSort").parent().siblings().find("span").removeClass("up");
+  		$(".dateSort").find("span").toggleClass("up");
+  		return false;
+  	});
 
-      $(".dateSort").click(function(){
-      $(".dateSort").parent().siblings().removeClass("active");
-      $(".dateSort").parent().addClass("active");
-      $(".dateSort").parent().siblings().find("span").removeClass("up");
-      $(".dateSort").find("span").toggleClass("up");
-      return false;
-      });
+  	// click events for handling propper css classes on "all items"/"my items" buttons
+  	$(".types li a").click(function(){
+  		$(this).parent().siblings().removeClass("active");
+  		$(this).parent().addClass("active");
 
-      // click events for handling propper css classes on "all items"/"my items" buttons
-      $(".types li a").click(function(){
-      $(this).parent().siblings().removeClass("active");
-      $(this).parent().addClass("active");
+  		return false;
+  	});
 
-      return false;
-      });
+  	// fancy dropdowns for filters, and handling special cases
+  	$("#mediumFilter").msDropDown();
+  	$("#mediumFilter").change(function(){
+  		if ($(this).val() =="more") $("#browserFiltersDropdown").show("slide");
+  		else   $("#browserFiltersDropdown").hide("slide");
+  	});
 
-      // fancy dropdowns for filters, and handling special cases
-      $("#mediumFilter").msDropDown();
-      $("#mediumFilter").change(function(){
-      if ($(this).val() =="more") $("#browserFiltersDropdown").show("slide");
-      else   $("#browserFiltersDropdown").hide("slide");
-      });
+  	$("#browserFiltersDropdown .cancelButton ").click(function(){$("#browserFiltersDropdown").hide("slide");});
 
-      $("#browserFiltersDropdown .cancelButton ").click(function(){$("#browserFiltersDropdown").hide("slide");});
+  	$("#collectionFilter").msDropDown();
+  	$("#collectionFilter").change(function(){
+  		if ($(this).val() =="more") window.location = "generic.html"
+  	});
+  	$("#nameFilter").msDropDown();
+  	$("#nameFilter").change(function(){
+  		if ($(this).val() =="more") window.location = "generic.html"
+  	});
+  	$("#subjectFilter").msDropDown();
+  	$("#periodFilter").msDropDown();
 
-      $("#collectionFilter").msDropDown();
-      $("#collectionFilter").change(function(){
-      if ($(this).val() =="more") window.location = "generic.html"
-      });
-      $("#nameFilter").msDropDown();
-      $("#nameFilter").change(function(){
-      if ($(this).val() =="more") window.location = "generic.html"
-      });
-      $("#subjectFilter").msDropDown();
-      $("#periodFilter").msDropDown();
+  		$('a[title]').qtip({ 
+  						   	   style: { 
+  							   name: 'dark', 
+  							   tip: true,
+  							    border: {
+  										 radius: 8
+  									  }
+  								}, 
+  							   position: {
+  								  corner: {
+  									 target: 'topMiddle',
+  									 tooltip: 'bottomMiddle'
+  								  }
+  						   		} 
+  		}); 
+
+  		$('.item').hover(function(){
+  			$(this).find('a.title').animate({"bottom":"0px"},200);
+  		}, function(){
+  			$(this).find('a.title').animate({"bottom":"-20px"},200);							
+  		});
+
+  		$("#indexLinks a").click(function(){
+
+  			// if use is clicking on an already active index, then most likely they 
+  			// want to close the index panel
+  			if ($(this).hasClass('active')){
+  				$("#browserFiltersDropdown").slideUp();
+  				$(this).removeClass('active').parent().removeClass('active');
+  			}
+
+  			// if user clicks on an index that's not already active, then give the clicked link
+  			// the 'active' class, and if necessary open the index panel
+  			else {
+  				$(this).parent().siblings().removeClass('active').find('a').removeClass('active');
+  				$(this).addClass('active').parent().addClass('active');
+  				if ($("#browserFiltersDropdown").css('display')=='none'){
+  					$("#browserFiltersDropdown").slideDown();
+  				}
+
+  			}
+  			return false;
+  		});
+
+  		$(".hideIndexPanel").click(function(){
+  			$("#browserFiltersDropdown").slideUp();
+  			$("#indexLinks a").removeClass('active').parent().removeClass('active');
+  			return false;
+  		});
 
 
-      // tooltips
-      $('a[title]').qtip({
-      style: {
-      name: 'dark',
-      tip: true,
-      border: {
-      radius: 8
-      }
-      },
-      position: {
-      corner: {
-      target: 'topMiddle',
-      tooltip: 'bottomMiddle'
-      }
-      }
-      });
-
-      // thumbnails in grid mode
-      $('a[rel*=facebox]').facebox({
-      loading_image : '/images/loading.gif',
-      close_image   : '/images/closelabel.gif'
-      })
-
-
-      });
-    </script>
+  	});
+  	</script>
     }
   end
 
