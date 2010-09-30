6785dc1832958051d7a51a6740f03c92cee77bda
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-09-30 00:06:09 +0000

    Style working as presented.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,5 +2,8 @@ class Admin::AdminController < ApplicationController
   
   # adds basic security to all admin controllers that inherit from this one
   before_filter :admin_required
+
+  # assign the admin layout to all the admin pages which inherit from here
+  layout 'admin'
   
 end
--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -1,20 +1,3 @@
 module Admin::AdminHelper
-  def admin_head_content
-    return %{
-      <!-- hello -->
-      #{ javascript_include_tag 'fancybox/jquery.fancybox-1.3.1.pack.js' }
-      #{ javascript_include_tag 'jwysiqyg/jquery.wysiwyg.js' }
-      #{ javascript_include_tag 'hint.js' }
-      #{ javascript_include_tag 'jquery.visualize.js' }
-      #{ javascript_include_tag 'jquery.tipsy.js' }
-      #{ javascript_include_tag 'browser.js' }
-      #{ javascript_include_tag 'custom.js' }
-
-      #{ stylesheet_link_tag 'admin/screen.css', :media => 'screen' }
-      #{ stylesheet_link_tag 'admin/datepicker.css', :media => 'screen' }
-      #{ stylesheet_link_tag 'admin/tipsy.css', :media => 'screen' }
-      #{ stylesheet_link_tag 'admin/visualize.css', :media => 'screen' }
-      #{ stylesheet_link_tag '/javascripts/fancybox/jquery.fancybox-1.3.1.css', :media => 'screen' }
-    }
-  end
+  
 end
