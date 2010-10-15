a70a2332ed04dae7227497e704d305e4f1481bc0
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-10-15 23:56:21 +0000

    Fixing up the people forms

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -47,7 +47,7 @@ class Admin::PeopleController < Admin::AdminController
 
     respond_to do |format|
       if @person.save
-        format.html { redirect_to(@person, :notice => 'Person was successfully created.') }
+        format.html { redirect_to(admin_person_path(@person), :notice => 'Person was successfully created.') }
         format.xml  { render :xml => @person, :status => :created, :location => @person }
       else
         format.html { render :action => "new" }
@@ -63,7 +63,7 @@ class Admin::PeopleController < Admin::AdminController
 
     respond_to do |format|
       if @person.update_attributes(params[:person])
-        format.html { redirect_to(@person, :notice => 'Person was successfully updated.') }
+        format.html { redirect_to(admin_person_path(@person), :notice => 'Person was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -79,7 +79,7 @@ class Admin::PeopleController < Admin::AdminController
     @person.destroy
 
     respond_to do |format|
-      format.html { redirect_to(people_url) }
+      format.html { redirect_to(admin_people_url) }
       format.xml  { head :ok }
     end
   end
