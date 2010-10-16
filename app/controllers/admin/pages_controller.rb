aae12ba2cf99a8e58a01511c200d38a16aa2d5fe
cforcey <cforcey@370120a7-2b37-49ed-ada7-46ef3e716084>
2010-10-16 04:57:51 +0000

    Administrative pages for "pages" completed.

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,6 +1,4 @@
-class PagesController < Admin::AdminController
-
-  before_filter :admin_required, :except => [:page]
+class Admin::PagesController < Admin::AdminController
 
   # GET /pages
   # GET /pages.xml
@@ -47,7 +45,7 @@ class PagesController < Admin::AdminController
 
     respond_to do |format|
       if @page.save
-        format.html { redirect_to(@page, :notice => 'Page was successfully created.') }
+        format.html { redirect_to(admin_page_path(@page), :notice => 'Page was successfully created.') }
         format.xml  { render :xml => @page, :status => :created, :location => @page }
       else
         format.html { render :action => "new" }
@@ -63,7 +61,7 @@ class PagesController < Admin::AdminController
 
     respond_to do |format|
       if @page.update_attributes(params[:page])
-        format.html { redirect_to(@page, :notice => 'Page was successfully updated.') }
+        format.html { redirect_to(admin_page_path(@page), :notice => 'Page was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -79,7 +77,7 @@ class PagesController < Admin::AdminController
     @page.destroy
 
     respond_to do |format|
-      format.html { redirect_to(pages_url) }
+      format.html { redirect_to(admin_pages_url) }
       format.xml  { head :ok }
     end
   end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
   translates :title, :caption, :body, :author
-
   default_scope :include => :translations
+  globalize_accessors :en, :fa
 end
