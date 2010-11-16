540ddf4c3be7d80bf742a7e324f471d9e35de0fe
Charles Forcey <cforcey@historicusinc.com>
2010-11-16 11:05:46 -0500

    Improving translation and label syntax on contact form to move translated text out of the code and into the text of the label field.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   validates :name, :presence => true, :length => {:maximum => 255}
   validates :email, :presence => true, :length => {:maximum => 255}
   validates :body, :presence => true, :length => {:maximum => 2000}
+  validates :math_problem, :presence => true, :numericality => {:equal_to => 2}
+  
+  attr_accessor :math_problem
 end
