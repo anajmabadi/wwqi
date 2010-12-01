26beaf5b79bc451de47a98f49012f849f687daa8
Charles Forcey <cforcey@historicusinc.com>
2010-12-01 11:33:07 -0500

    Fixing up migration (though oddly I had already  created the table in an alternate universe)

--- db/migrate/20101201153745_create_eras.rb
+++ db/migrate/20101201153745_create_eras.rb
@@ -1,15 +1,16 @@
 class CreateEras < ActiveRecord::Migration
   def self.up
     create_table :eras do |t|
-      t.string :title
       t.integer :year
       t.boolean :publish
 
       t.timestamps
     end
+    Era.create_translation_table! :title => :string
   end
 
   def self.down
+    Era.drop_translation_table! 
     drop_table :eras
   end
 end
