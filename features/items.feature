3a6b38abe93cec26b2a549500ff882cd2a0a4f7f
Charles Forcey <cforcey@historicusinc.com>
2010-12-07 20:06:16 -0500

    testing refactored to allow add and edit but no delete yet because of javascript.

--- features/items.feature
+++ features/items.feature
@@ -15,3 +15,14 @@ Feature: an authorized user should be able to CRUD an item
 			And I follow "Sample Item"
 			Then I should be on the item page for "Sample Item"
 			And I should see "Items | Show"
+			
+    Scenario: Adding an item to be published
+      Given I speak English
+      When I create an item named Sample Item to be published
+      Then "Sample Item" should be published
+   
+    Scenario: Editing an item 
+      Given I speak English
+      And I have an item titled "Sample Item"
+      When I edit an item titled "Sample Item"
+      Then I should have an item titled "New Title"
--- features/step_definitions/item_steps.rb
+++ features/step_definitions/item_steps.rb
@@ -1,3 +1,36 @@
 Given /^I have an item titled "([^"]*)"$/ do |arg1|
-  @item = Item.create!(:title => arg1, :pages => 1, :accession_num => "abcdefg")
+  @item = Item.create!(:title => arg1, :pages => 1, :accession_num => "abcdefg", :publish => true)
+end
+
+When /^I create an item named Sample Item to be published$/ do
+  visit admin_items_path
+  click_link 'New'
+  fill_in 'Title en', :with => 'Sample Item'
+  fill_in 'Title fa', :with => 'Sample Item fa'
+  fill_in 'Accession num', :with => 'abcdf'
+  check 'Publish'
+  click_button 'Create Item'
+end
+
+Then /^"([^"]*)" should be published$/ do |arg1|
+  visit admin_items_path
+  click_link "Sample Item"
+  response.should contain("abcdf")
+  response.should contain("Sample Item")
+  response.should contain("Publish: true")
+end
+
+When /^I edit an item titled "([^"]*)"$/ do |arg1|
+  visit admin_items_path
+  click_link "Sample Item"
+  click_link 'Edit'
+  fill_in 'Title en', :with => 'New Title'
+  click_button 'Update Item'
+end
+
+Then /^I should have an item titled "([^"]*)"$/ do |arg1|
+  visit admin_items_path
+  click_link arg1
+  response.should contain(arg1)
+  response.should contain("Publish: true")
 end
