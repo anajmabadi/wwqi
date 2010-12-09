df9317e690ce7246dbaa07590ad43446a089cf96
Charles Forcey <cforcey@historicusinc.com>
2010-12-09 02:33:49 -0500

    fixing trailing 0 on dimensions with helper function

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ActiveRecord::Base
   
   # truncate and other helpers
   include ActionView::Helpers::TextHelper
+  include ApplicationHelper
 
 
   belongs_to :owner
@@ -265,9 +266,9 @@ class Item < ActiveRecord::Base
   
   def dimension_label
     dimensions_set = []
-    dimensions_set << localize_number(self.width) unless self.width.nil? || self.width == 0
-    dimensions_set << localize_number(self.height) unless self.height.nil? || self.height == 0
-    dimensions_set << localize_number(self.depth) unless self.depth.nil? || self.depth == 0
+    dimensions_set << localize_number(floatstrip(self.width)) unless self.width.nil? || self.width == 0
+    dimensions_set << localize_number(floatstrip(self.height)) unless self.height.nil? || self.height == 0
+    dimensions_set << localize_number(floatstrip(self.depth)) unless self.depth.nil? || self.depth == 0
     label = dimensions_set.join(" #{I18n.translate(:dimension_separator)} ") + ' ' + I18n.translate(:dimension_unit) unless dimensions_set.empty?
     return label
   end
