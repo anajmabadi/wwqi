8375c2df04681b9cf616498738b4500a2d669043
Charles Forcey <cforcey@historicusinc.com>
2010-12-09 03:51:35 -0500

    fixing up appearance menu so it shows English and persian

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -5,4 +5,10 @@ class Appearance < ActiveRecord::Base
   translates :caption
   globalize_accessors :en, :fa
   default_scope :include => [:translations]
+  
+  def to_label
+    my_label = "#{self.person_id.to_s}. #{self.person.name_fa}/#{self.person.name_en}" unless self.person.name_en.nil?
+    my_label += " (#{self.caption})" unless self.caption.nil?
+    return my_label
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -10,11 +10,10 @@ class Person < ActiveRecord::Base
   translates :name, :sort_name, :description, :vitals, :birth_place
   globalize_accessors :fa, :en
   default_scope :include => :translations
-  
   def self.select_list
-    return self.all(:conditions => ['person_translations.locale = ?', I18n.locale.to_s], :select => 'DISTINCT id, person_translations.name', :order => 'person_translations.sort_name').map {|person| [person.name, person.id]}
+    return self.all(:select => 'DISTINCT id, person_translations.name', :order => 'person_translations.sort_name').map {|person| [person.to_label, person.id]}.sort
   end
-  
+
   def self.select_list_fa
     return self.all(:conditions => "person_translations.locale = 'fa'", :select => 'DISTINCT id, person_translations.name', :order => 'person_translations.sort_name').map {|person| [person.name, person.id]}
   end
@@ -24,8 +23,13 @@ class Person < ActiveRecord::Base
     value += ' (' + vitals + ')' unless vitals.blank?
     return value
   end
-  
+
   def collections
-      return Collection.find(self.items.map { |item| collection_id }.uniq.sort) unless self.items.empty?
+    return Collection.find(self.items.map { |item| collection_id }.uniq.sort) unless self.items.empty?
+  end
+
+  def to_label
+    my_label = "#{self.name_en}/#{self.name_fa} (#{self.id.to_s})" unless self.name_en.nil?
+    return my_label
   end
 end
