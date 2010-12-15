c68b54844e999f4b17ccb0b2476d96f232429096
Charles Forcey <cforcey@historicusinc.com>
2010-12-15 06:07:28 -0500

    Data hardening of appearances model to avoid duplicates.

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -6,6 +6,11 @@ class Appearance < ActiveRecord::Base
   globalize_accessors :en, :fa
   default_scope :include => [:translations]
   
+  validates :publish, :presence => true
+  validates :person_id, :presence => true, :uniqueness => {:scope => :item_id}
+  validates :item_id, :presence => true
+  validates :position, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
+  
   def to_label
     my_label = self.person.to_label unless self.person.nil?
     my_label += " (#{self.caption})" unless self.caption.blank?
--- spec/models/appearance_spec.rb
+++ spec/models/appearance_spec.rb
@@ -1,5 +1,57 @@
 require 'spec_helper'
 
 describe Appearance do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @appearance = Appearance.new
+    @sample_attributes = {
+      :item_id => 1,
+      :person_id => 2,
+      :position => 1,
+      :publish => true,
+      :caption => 'Sample caption',
+      :notes => 'sample notes'
+    }
+  end
+
+  it "should be valid" do
+    @appearance.update_attributes(@sample_attributes)
+    @appearance.should be_valid
+  end
+  
+  it "should require an item_id" do
+    @sample_attributes[:item_id] = nil
+    @appearance.update_attributes(@sample_attributes)
+    @appearance.should_not be_valid
+  end
+  
+  it "should require a person_id" do
+    @sample_attributes[:person_id] = nil
+    @appearance.update_attributes(@sample_attributes)
+    @appearance.should_not be_valid
+  end
+  
+  it "should require a position" do
+    @sample_attributes[:position] = nil
+    @appearance.update_attributes(@sample_attributes)
+    @appearance.should_not be_valid
+  end
+  
+  it "should require a numerical position" do  
+    @sample_attributes[:position] = 'a'
+    @appearance.update_attributes(@sample_attributes)
+    @appearance.should_not be_valid
+  end
+  
+  it "should require a publish choice" do  
+    @sample_attributes[:publish] = nil
+    @appearance.update_attributes(@sample_attributes)
+    @appearance.should_not be_valid
+  end
+  
+  it "should not allow duplicate persons and items" do  
+    @appearance.update_attributes(@sample_attributes)
+    @appearance.should be_valid
+    @appearance2 = Appearance.new(@sample_attributes)
+    @appearance2.should_not be_valid
+  end
 end
