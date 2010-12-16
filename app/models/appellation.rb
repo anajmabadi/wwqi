020b836be57aeff8d5bbdf7f7655ec01bfcd4412
Charles Forcey <cforcey@historicusinc.com>
2010-12-16 13:23:00 -0500

    moving menu

--- app/models/appellation.rb
+++ app/models/appellation.rb
@@ -5,5 +5,6 @@ class Appellation < ActiveRecord::Base
   globalize_accessors :fa, :en
   default_scope :include => :translations
   
-  
+  validates :name, :presence => true, :length => {:maximum => 255}
+  validates :sort_name, :length => {:maximum => 255}
 end
--- spec/models/appellation_spec.rb
+++ spec/models/appellation_spec.rb
@@ -1,5 +1,57 @@
 require 'spec_helper'
 
 describe Appellation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @appellation = Appellation.new
+    @sample_attributes = {
+      :person_id => 1,
+      :position => 1,
+      :publish => true,
+      :name => 'Sample name',
+      :sort_name => 'Sort name',
+      :notes => 'sample notes'
+    }
+  end
+
+  it "should be valid" do
+    @appellation.update_attributes(@sample_attributes)
+    @appellation.should be_valid
+  end
+  
+  it "should require an person_id" do
+    @sample_attributes[:person_id] = nil
+    @appellation.update_attributes(@sample_attributes)
+    @appellation.should_not be_valid
+  end
+  
+  it "should require a person_id" do
+    @sample_attributes[:person_id] = nil
+    @appellation.update_attributes(@sample_attributes)
+    @appellation.should_not be_valid
+  end
+  
+  it "should require a position" do
+    @sample_attributes[:position] = nil
+    @appellation.update_attributes(@sample_attributes)
+    @appellation.should_not be_valid
+  end
+  
+  it "should require a numerical position" do  
+    @sample_attributes[:position] = 'a'
+    @appellation.update_attributes(@sample_attributes)
+    @appellation.should_not be_valid
+  end
+  
+  it "should require a publish choice" do  
+    @sample_attributes[:publish] = nil
+    @appellation.update_attributes(@sample_attributes)
+    @appellation.should_not be_valid
+  end
+  
+  it "should not allow duplicate appellations" do  
+    @appellation.update_attributes(@sample_attributes)
+    @appellation.should be_valid
+    @appellation2 = Appearance.new(@sample_attributes)
+    @appellation2.should_not be_valid
+  end
 end
