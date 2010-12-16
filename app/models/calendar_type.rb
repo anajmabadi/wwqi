4f0c388e85b37f2c3721a83088b388500b8345cc
Charles Forcey <cforcey@historicusinc.com>
2010-12-16 14:22:38 -0500

    validating calendar types

--- app/models/calendar_type.rb
+++ app/models/calendar_type.rb
@@ -1,6 +1,9 @@
 class CalendarType < ActiveRecord::Base
   has_many :items
 
+  validates :name, :presence => true, :uniqueness => true
+  validates :publish, :presence => true
+
   def self.select_list
     return self.all(:select => 'DISTINCT id, name', :order => 'name').map {|calendar_type| [calendar_type.name, calendar_type.id]}
   end
