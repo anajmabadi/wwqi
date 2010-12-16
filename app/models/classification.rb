1c4eb5ec5c711639fceb7a49cb2ebf1354a108e2
Charles Forcey <cforcey@historicusinc.com>
2010-12-16 14:35:12 -0500

    touching up classification validations

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,11 +2,10 @@ class Classification < ActiveRecord::Base
   belongs_to :item, :include => :translations
   belongs_to :subject, :include => :translations
 
-  validates :item_id, :presence => true, :numericality => true
+  validates :item_id, :presence => true, :numericality => true, :uniqueness => {:scope => :item_id}
   validates :subject_id, :presence => true, :numericality => true
   validates :publish, :presence => true
   validates :position, :presence => true, :numericality => true
-  validates_uniqueness_of :item_id, :scope => :subject_id
 
   # pagination code
   cattr_reader :per_page
--- spec/models/classification_spec.rb
+++ spec/models/classification_spec.rb
@@ -1,5 +1,56 @@
 require 'spec_helper'
 
 describe Classification do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @classification = Classification.new
+    @sample_attributes = {
+      :item_id => 1,
+      :subject_id => 1,
+      :position => 1,
+      :publish => true,
+      :notes => 'sample notes'
+    }
+  end
+
+  it "should be valid" do
+    @classification.update_attributes(@sample_attributes)
+    @classification.should be_valid
+  end
+  
+  it "should require a item_id" do  
+    @sample_attributes[:item_id] = nil
+    @classification.update_attributes(@sample_attributes)
+    @classification.should_not be_valid
+  end
+  
+  it "should require a subject_id" do  
+    @sample_attributes[:item_id] = nil
+    @classification.update_attributes(@sample_attributes)
+    @classification.should_not be_valid
+  end
+  
+  it "should require a publish choice" do  
+    @sample_attributes[:publish] = nil
+    @classification.update_attributes(@sample_attributes)
+    @classification.should_not be_valid
+  end
+    
+  it "should require a position" do
+    @sample_attributes[:position] = nil
+    @classification.update_attributes(@sample_attributes)
+    @classification.should_not be_valid
+  end
+  
+  it "should require a numerical position" do  
+    @sample_attributes[:position] = 'a'
+    @classification.update_attributes(@sample_attributes)
+    @classification.should_not be_valid
+  end
+  
+  it "should not allow duplicate calendar types and items" do  
+    @classification.update_attributes(@sample_attributes)
+    @classification.should be_valid
+    @classification2 = Classification.new(@sample_attributes)
+    @classification2.should_not be_valid
+  end
 end
