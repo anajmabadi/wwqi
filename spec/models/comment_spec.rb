0b73a3b3b2cf047d045dc49e405faf9187ec3128
Charles Forcey <cforcey@historicusinc.com>
2010-12-17 09:07:31 -0500

    Fixing up comment validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,47 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before(:each) do
+    @comment = Comment.new
+    @sample_attributes = {
+      :name => "MyName",
+      :email => "cforcey@historicusinc.com",
+      :body => "My Body",
+      :subject => "My Caption",
+      :replied_at => Date.new,
+      :submitted_at => Date.new,
+      :ip => "1.1.1.1",
+      :notes => 'sample notes',
+      :math_problem => 2
+    }
+  end
+
+  it "should be valid" do
+    @comment.update_attributes(@sample_attributes)
+    @comment.should be_valid
+  end
+  
+  it "should require a name" do  
+    @sample_attributes[:name] = nil
+    @comment.update_attributes(@sample_attributes)
+    @comment.should_not be_valid
+  end
+  
+  it "should require an email" do  
+    @sample_attributes[:email] = nil
+    @comment.update_attributes(@sample_attributes)
+    @comment.should_not be_valid
+  end
+  
+  it "should require a body" do  
+    @sample_attributes[:body] = nil
+    @comment.update_attributes(@sample_attributes)
+    @comment.should_not be_valid
+  end
+
+  it "should require a correct math answer" do  
+    @sample_attributes[:math_problem] = 99
+    @comment.update_attributes(@sample_attributes)
+    @comment.should_not be_valid
+  end    
 end
