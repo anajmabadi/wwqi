e477a386abc4879ddb4472953a74385a0d5ae487
Charles Forcey <cforcey@historicusinc.com>
2010-12-20 07:22:08 -0500

    basic validation on repositories with spec

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,13 +4,19 @@ class Repository < ActiveRecord::Base
   include ActionView::Helpers::TextHelper
   
   belongs_to :owner
-  has_many :passports, :order => :position
+  has_many :passports, :order => :position, :dependent => :destroy
   has_many :items, :through => :passports
 
   # globalize
   translates :name
   globalize_accessors :fa, :en
   default_scope :include => [:translations]
+  
+  validates :name_en, :presence => true, :length => {:maximum => 255} 
+  validates :name_fa, :presence => true, :length => {:maximum => 255} 
+  validates :url, :presence => true, :length => {:maximum => 255}, :uniqueness => true
+  validates :publish, :inclusion => { :in => [true,false] }
+  validates :owner_id, :numericality => true, :presence => true
 
   def to_label
     return truncate(self.name) + " (#{self.id.to_s})"
--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -1,5 +1,51 @@
 require 'spec_helper'
 
 describe Repository do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before(:each) do
+    @repository = Repository.new
+    @sample_attributes = {
+      :name_en => 'Hello Calendar Type',
+      :name_fa => 'Hello Persian',
+      :url => 'url for me',
+      :owner_id => '3',
+      :publish => true,
+      :notes => 'sample notes'
+    }
+  end
+
+  it "should be valid" do
+    @repository.update_attributes(@sample_attributes)
+    @repository.should be_valid
+  end
+  
+  it "should require an English name" do  
+    @sample_attributes[:name_en] = nil
+    @repository.update_attributes(@sample_attributes)
+    @repository.should_not be_valid
+  end
+  
+  it "should require a Persian name" do  
+    @sample_attributes[:name_fa] = nil
+    @repository.update_attributes(@sample_attributes)
+    @repository.should_not be_valid
+  end
+  
+  it "should require a publish choice" do  
+    @sample_attributes[:publish] = nil
+    @repository.update_attributes(@sample_attributes)
+    @repository.should_not be_valid
+  end
+  
+ it "should require an owner id " do  
+    @sample_attributes[:owner_id] = nil
+    @repository.update_attributes(@sample_attributes)
+    @repository.should_not be_valid
+  end
+  
+  it "should not allow duplicate repositories" do  
+    @repository.update_attributes(@sample_attributes)
+    @repository.should be_valid
+    @repository2 = Repository.new(@sample_attributes)
+    @repository2.should_not be_valid
+  end
 end
