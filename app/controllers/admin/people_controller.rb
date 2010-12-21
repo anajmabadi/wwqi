1a6e4a6ff08463e375e9b989bf7fa8317e5ec140
Charles Forcey <cforcey@historicusinc.com>
2010-12-20 21:43:59 -0500

    basic people keyword search and sorting -- dealing wiht special language case

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -5,7 +5,29 @@ class Admin::PeopleController < Admin::AdminController
   # GET /people
   # GET /people.xml
   def index
-    @people = Person.all
+    
+    @order = sort_order('person_translations.name')
+    
+    # look for filters
+    @keyword_filter = params[:keyword_filter] unless params[:keyword_filter] == I18n.translate(:search_prompt)
+
+    # unless @keyword_filter.nil? && @collection_filter.nil? && period_filer.nil? && subject_type_filter.nil?
+
+    @query_hash = { :conditions => [], :parameters => {} }
+    @query_hash = build_keyword_query(@keyword_filter, @query_hash) unless @keyword_filter.blank? || @keyword_filter == I18n.translate(:search_prompt)
+
+    # assemble the query from the two sql injection safe parts
+    @query_conditions = ''
+    @query_hash[:conditions].each do |condition|
+      @query_conditions += (@query_conditions.blank? ? '': ' AND ') + condition
+    end
+
+    @query = [@query_conditions, @query_hash[:parameters]]
+
+    @people = Person.where(@query).order(@order)
+
+    #cache the current search set in a session variable
+    session[:admin_people_index_url] = request.fullpath
 
     respond_to do |format|
       format.html # index.html.erb
@@ -82,5 +104,26 @@ class Admin::PeopleController < Admin::AdminController
       format.html { redirect_to(admin_people_url) }
       format.xml  { head :ok }
     end
+  end
+  
+  
+  private
+
+  def build_keyword_query(filter_value, query_hash)
+    additional_query = ''
+    filter_value = filter_value.lstrip
+    filter_value = filter_value.length > 256 ? filter_value[0..255] : filter_value
+    filter_value = filter_value.upcase #locale insensitive
+    filter_value = "%#{filter_value}%"
+    # ucase if it English
+    if I18n.locale == :en
+      additional_query += "CONCAT_WS('|', UPPER(person_translations.name), UPPER(person_translations.sort_name), UPPER(people.loc_name), CONCAT('ID',people.id)) LIKE :keyword" unless filter_value.blank?
+    else
+      additional_query += "CONCAT_WS('|',person_translations.name, person_translations.sort_name, people.loc_name, people.id) LIKE :keyword" unless filter_value.blank?
+    end
+
+    query_hash[:conditions] << additional_query
+    query_hash[:parameters][:keyword] = filter_value
+    return query_hash
   end
 end
