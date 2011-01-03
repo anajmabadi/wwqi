8490ba86b9abd553938484d6465180bfa4b3525f
Charles Forcey <cforcey@historicusinc.com>
2011-01-02 19:47:21 -0500

    Enabling collection and genre filters

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -145,11 +145,8 @@ class ArchiveController < ApplicationController
       @file_to_send = @item.zip_path
       unless File.exists?(@file_to_send)
         #create a zip file if it is the first time
-        Rails.logger.error "made it into the loop"
         zip_them_all = ZipThemAll.new(@file_to_send, @item.preview_paths)
-        Rails.logger.error "Did it work: " + zip_them_all.zip_file_path
         zip_them_all.zip
-        Rails.logger.error "Does zip exist: " + File.exists?(@file_to_send).to_s
       end
       send_file @file_to_send, :type => "application/zip"
     rescue => error
