c89f491925a11ec73b9f5deaeb0a8d9983ca8b6d
Charles Forcey <cforcey@historicusinc.com>
2011-01-09 09:14:35 -0500

    change in administrative password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -77,7 +77,7 @@ class ApplicationController < ActionController::Base
   #basis security
   def admin_required
     authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == 'admin' && password == 'Qajar'
+      user_name == 'Editor2011' && password == 'Q@j@r2011'
     end if Rails.env == 'production' || params[:admin_http]
   end
   
