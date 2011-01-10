cbc081d8519b5675d878e9f03c156647b2dc321a
Charles Forcey <cforcey@historicusinc.com>
2011-01-10 15:40:48 -0500

    adding index display fields, keyword search, and sorting to places display.

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -2,7 +2,31 @@ class Admin::PlacesController < Admin::AdminController
   # GET /places
   # GET /places.xml
   def index
-    @places = Place.all
+    
+    # set the default order
+    params[:c] = 'name_en' if params[:c].blank?
+    @order = sort_order('places.id') unless params[:c] == 'name_en' || params[:c] == 'name_fa'
+
+    # look for filters
+    @keyword_filter = params[:keyword_filter] unless params[:keyword_filter] == I18n.translate(:search_prompt)
+    
+    @query_hash = { :conditions => [], :parameters => {} }
+    @query_hash = build_keyword_query(@keyword_filter, @query_hash) unless @keyword_filter.blank? || @keyword_filter == I18n.translate(:search_prompt)
+    
+    # assemble the query from the two sql injection safe parts
+    @query_conditions = ''
+    @query_hash[:conditions].each do |condition|
+      @query_conditions += (@query_conditions.blank? ? '': ' AND ') + condition
+    end
+
+    @query = [@query_conditions, @query_hash[:parameters]]
+
+    @places = Place.where(@query).order(@order)
+
+    @places = sort_bilingual(@places, params[:c], params[:d]) if ["name_en", "name_fa"].include?(params[:c])
+
+    #cache the current search set in a session variable
+    session[:admin_places_index_url] = request.fullpath
 
     respond_to do |format|
       format.html # index.html.erb
@@ -80,4 +104,31 @@ class Admin::PlacesController < Admin::AdminController
       format.xml  { head :ok }
     end
   end
+  
+  private
+  
+  def build_keyword_query(filter_value, query_hash)
+    additional_query = ''
+    filter_value = filter_value.lstrip
+    filter_value = filter_value.length > 256 ? filter_value[0..255] : filter_value
+    filter_value = filter_value.upcase #locale insensitive
+    filter_value = "%#{filter_value}%"
+    keyword_query = "UPPER(place_translations.name) LIKE :keyword" unless filter_value.blank?
+    ids = Place.where([keyword_query, {:keyword => filter_value}]).select('places.id').order('places.id').map { |i| i.id }
+    additional_query += "places.id IN (:keyword_ids)"
+    query_hash[:conditions] << additional_query
+    query_hash[:parameters][:keyword_ids] = ids
+    return query_hash
+  end
+
+  def sort_bilingual(rows, bilingual_field, direction)
+    rows = case bilingual_field
+      when 'name_en' then rows.sort_by(&:name_en)
+      when 'name_fa' then rows.sort_by(&:name_fa)
+    else rows
+    end
+    rows.reverse! if direction == 'down'
+    return rows
+  end
+
 end
