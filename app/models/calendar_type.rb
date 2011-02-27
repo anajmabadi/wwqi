ab1716146f42f99bae0f0a6aea631a3d8f33f496
Charles Forcey <cforcey@historicusinc.com>
2011-02-27 10:21:57 -0500

    turning calendar_types into a translated table

--- app/models/calendar_type.rb
+++ app/models/calendar_type.rb
@@ -4,7 +4,12 @@ class CalendarType < ActiveRecord::Base
 
   validates :name, :presence => true, :uniqueness => true
   validates :publish, :inclusion => { :in => [true,false] }
-
+  
+  # globalize2 accessors 
+  translates :name
+  globalize_accessors :fa, :en
+  default_scope :include => [:translations]
+  
   def self.select_list
     return self.all(:select => 'DISTINCT id, name', :order => 'name').map {|calendar_type| [calendar_type.name, calendar_type.id]}
   end
