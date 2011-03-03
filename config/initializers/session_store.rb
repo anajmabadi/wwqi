823ac64ee849f9dcdb290d2e40cd639b6cc246f3
Charles Forcey <cforcey@historicusinc.com>
2011-03-02 23:27:01 -0500

    After thousands of experiments, returning to active_record_store since it was not the culprit in the missing my archive cookies.  Did add any domain which might help retaining state.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-#Qajar::Application.config.session_store :cookie_store, :key => '_qajar_session', :domain => '.qajar_women.local'
+#Qajar::Application.config.session_store :cookie_store, :key => '_qajar_women', :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-Qajar::Application.config.session_store :active_record_store, :key => '_qajar_session', :domain => '.qajarwomen.org'
+Qajar::Application.config.session_store :active_record_store, :domain => :all
