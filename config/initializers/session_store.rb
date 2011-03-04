71af20724d43395ccd0164b9012f563d2a5d325d
Charles Forcey <cforcey@historicusinc.com>
2011-03-04 16:04:01 -0500

    adjusting cookies for firefox and chrome

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-Qajar::Application.config.session_store :active_record_store, :domain => :all
+Qajar::Application.config.session_store :active_record_store, :domain => '.qajarwomen.org'
