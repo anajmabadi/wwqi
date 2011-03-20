616f149cad72d3de17e39dcc53a483dd0a07e6db
Charles Forcey <cforcey@historicusinc.com>
2011-03-20 15:25:58 -0400

    clips csv handlers

--- app/controllers/admin/clips_controller.rb
+++ app/controllers/admin/clips_controller.rb
@@ -1,101 +1,125 @@
 class Admin::ClipsController < Admin::AdminController
-  # GET /clips
-  # GET /clips.xml
-  def index
-    @order =  sort_order('item_id')
-    @clips = Clip.includes(:item, :clip_type).order(@order)
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @clips }
-    end
-  end
+	# GET /clips
+	# GET /clips.xml
+	def index
+		@order =  sort_order('item_id')
+		@clips = Clip.includes(:item, :clip_type).order(@order)
+		respond_to do |format|
+			format.html # index.html.erb
+			format.xml  { render :xml => @clips }
+		end
+	end
 
-  # GET /clips/1
-  # GET /clips/1.xml
-  def show
-    @clip = Clip.find(params[:id])
+	def export
+		begin
+			@order =  sort_order('item_id')
+			@clips = Clip.includes(:item, :clip_type).order(@order)
+		rescue => error
+			flash[:error] = "There was a problem finding clips: " + error.message
+			@error = true
+		end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @clip }
-    end
-  end
+		respond_to do |format|
+			format.html { redirect_to admin_clips_path, :error => flash[:error] }
+			format.csv do
+				csv_string = make_custom_csv(@clips)
+				# send it to the browsah
+				send_data csv_string,
+                :type => 'text/csv; charset=utf-8; header=present',
+                :disposition => "attachment; filename=clips.csv"
+			end
+			format.xml  { render :xml => @clips }
+		end
 
-  # GET /clips/new
-  # GET /clips/new.xml
-  def new
-    @clip = Clip.new
-    @items = items_list
-    @owners = Owner.select_list
-    @clip_types = clip_types_list
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @clip }
-    end
-  end
+	end
 
-  # GET /clips/1/edit
-  def edit
-    @clip = Clip.find(params[:id])
-    @items = items_list
-    @owners = Owner.select_list
-    @clip_types = clip_types_list
-  end
+	# GET /clips/1
+	# GET /clips/1.xml
+	def show
+		@clip = Clip.find(params[:id])
 
-  # POST /clips
-  # POST /clips.xml
-  def create
-    @clip = Clip.new(params[:clip])
-    @items = items_list
-    @clip_types = clip_types_list
-    @owners = Owner.select_list
-    respond_to do |format|
-      if @clip.save
-        format.html { redirect_to(admin_clip_path(@clip), :notice => 'Clip was successfully created.') }
-        format.xml  { render :xml => @clip, :status => :created, :location => @clip }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @clip.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			format.html # show.html.erb
+			format.xml  { render :xml => @clip }
+		end
+	end
 
-  # PUT /clips/1
-  # PUT /clips/1.xml
-  def update
-    @clip = Clip.find(params[:id])
-    @items = items_list
-    @clip_types = clip_types_list
-    @owners = Owner.select_list
-    respond_to do |format|
-      if @clip.update_attributes(params[:clip])
-        format.html { redirect_to(admin_clip_path(@clip), :notice => 'Clip was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @clip.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+	# GET /clips/new
+	# GET /clips/new.xml
+	def new
+		@clip = Clip.new
+		@items = items_list
+		@owners = Owner.select_list
+		@clip_types = clip_types_list
+		respond_to do |format|
+			format.html # new.html.erb
+			format.xml  { render :xml => @clip }
+		end
+	end
 
-  # DELETE /clips/1
-  # DELETE /clips/1.xml
-  def destroy
-    @clip = Clip.find(params[:id])
-    @clip.destroy
+	# GET /clips/1/edit
+	def edit
+		@clip = Clip.find(params[:id])
+		@items = items_list
+		@owners = Owner.select_list
+		@clip_types = clip_types_list
+	end
 
-    respond_to do |format|
-      format.html { redirect_to(admin_clips_path) }
-      format.xml  { head :ok }
-    end
-  end
+	# POST /clips
+	# POST /clips.xml
+	def create
+		@clip = Clip.new(params[:clip])
+		@items = items_list
+		@clip_types = clip_types_list
+		@owners = Owner.select_list
+		respond_to do |format|
+			if @clip.save
+				format.html { redirect_to(admin_clip_path(@clip), :notice => 'Clip was successfully created.') }
+				format.xml  { render :xml => @clip, :status => :created, :location => @clip }
+			else
+				format.html { render :action => "new" }
+				format.xml  { render :xml => @clip.errors, :status => :unprocessable_entity }
+			end
+		end
+	end
 
-  private
+	# PUT /clips/1
+	# PUT /clips/1.xml
+	def update
+		@clip = Clip.find(params[:id])
+		@items = items_list
+		@clip_types = clip_types_list
+		@owners = Owner.select_list
+		respond_to do |format|
+			if @clip.update_attributes(params[:clip])
+				format.html { redirect_to(admin_clip_path(@clip), :notice => 'Clip was successfully updated.') }
+				format.xml  { head :ok }
+			else
+				format.html { render :action => "edit" }
+				format.xml  { render :xml => @clip.errors, :status => :unprocessable_entity }
+			end
+		end
+	end
 
-  def items_list
-    return Item.select(['item_translations.title', 'items.id']).order('items.id').map { |i| [ "#{i.id.to_s}: #{ i.title}", i.id] }
-  end
-  def clip_types_list
-    return ClipType.select(['name', 'id']).order('name').map { |c| [c.name, c.id] }
-  end
+	# DELETE /clips/1
+	# DELETE /clips/1.xml
+	def destroy
+		@clip = Clip.find(params[:id])
+		@clip.destroy
+
+		respond_to do |format|
+			format.html { redirect_to(admin_clips_path) }
+			format.xml  { head :ok }
+		end
+	end
+
+	private
+
+	def items_list
+		return Item.select(['item_translations.title', 'items.id']).order('items.id').map { |i| [ "#{i.id.to_s}: #{ i.title}", i.id] }
+	end
+
+	def clip_types_list
+		return ClipType.select(['name', 'id']).order('name').map { |c| [c.name, c.id] }
+	end
 end
--- app/models/clip.rb
+++ app/models/clip.rb
@@ -17,6 +17,31 @@ class Clip < ActiveRecord::Base
   validates :publish, :inclusion => { :in => [true,false] }
   validates :duration, :presence => true, :numericality => {:greater_than => 0}
   
+  def csv_fields
+    return %w[	id title_fa title_en caption_fa caption_en
+    			interviewer_fa interviewer_en interviewee_fa interviewee_en
+    			location_fa location_en item_title_fa item_title_en  
+    			clip_type_id clip_type_name recorded_on duration 
+    			owner_name position publish notes]  
+  end
+  
+  def item_title_en
+  	return self.item.title_en unless self.item.nil?
+  end
+  
+  def item_title_fa
+  	return self.item.title_fa unless self.item.nil?
+  end
+  
+  def clip_type_name
+  	return self.clip_type.name unless self.clip_type.nil?
+  end
+  
+  def owner_name
+  	return self.owner.name unless self.owner.nil?
+  end
+  
+  
   # media file accessors from library
   def clip_file_name(format="wav")
     return "it_#{self.item_id.to_s}_#{self.position.to_s}_clip.#{format}" unless format.nil? || self.item_id.nil? || self.position.nil?
