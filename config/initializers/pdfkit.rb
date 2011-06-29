f8ba2dbafba28b77124a3f3eca8549f2c1538a3c
Brian Kaney <brian@vermonster.com>
2011-06-29 19:38:44 -0400

    Made path to wkhtmltopdf less brittle

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,6 +1,6 @@
 # config/initializers/pdfkit.rb
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
+  config.wkhtmltopdf = `which pdfkit`.strip
   config.default_options = {
     :page_size => 'Letter',
     :print_media_type => true,
