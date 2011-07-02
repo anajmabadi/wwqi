d53eb14257bc6806ca429a57eab8dc5389ac8007
Brian Kaney <brian@vermonster.com>
2011-07-02 00:15:48 -0400

    Made a more robust boostrap that works with or without /mnt.

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,7 +69,7 @@ namespace :deploy do
   end
 
   task :bootstrap_app_dir do
-    run 'sudo mkdir -p /srv/app; sudo chown ubuntu:ubuntu -R /srv/app;'
+    run 'if [ -e /mnt ] ; then sudo mkdir -p /mnt/app; sudo chown ubuntu:ubuntu -R /mnt/app; ln -nsf /mnt/app /srv/app; else sudo mkdir -p /srv/app; sudo chown ubunut:ubunut -R /srv/app; fi'
   end
 
 end
