49492a32bae428e07d0b1da76b696d1dbc04d1b4
Brian Kaney <brian@vermonster.com>
2011-07-06 13:29:03 -0400

    Added a reply-to

--- app/mailers/citation_mailer.rb
+++ app/mailers/citation_mailer.rb
@@ -1,11 +1,12 @@
 class CitationMailer < ActionMailer::Base
-  default :from => "info@qajarwomen.org"
+  default :from => "wwqi@fas.harvard.edu"
   
-  def email_citation(to,from,note,html_citations,text_citations)
+  def email_citation(to, from, note, html_citations, text_citations)
   	@note = note
   	@html_citations = html_citations
   	@text_citations = text_citations
   	subject = I18n.translate(:site_title) + ": " + I18n.translate(:email_citation)
-  	mail(:to => to, :from => from, :subject => @subject)
+  	mail(:to => to, :from => 'wwqi@fas.harvard.edu', :reply_to => from, :subject => @subject)
   end
+
 end
