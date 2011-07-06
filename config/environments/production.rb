b8c96da0777ebf5b8ca00d7f6ac8d4d0e36f1e64
Brian Kaney <brian@vermonster.com>
2011-07-06 13:20:11 -0400

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,6 @@ Qajar::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :ses
 
   # Enable threaded mode
   # config.threadsafe!
@@ -55,12 +54,15 @@ Qajar::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   :location => '/usr/sbin/sendmail',
   #   :arguments => '-i -t'
   # }
+  
+  config.action_mailer.delivery_method = :ses
+  
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 end
